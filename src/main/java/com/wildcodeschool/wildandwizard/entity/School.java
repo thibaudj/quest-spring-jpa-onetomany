976aa5b55c1d962cb8d5d90e7a28af138a11a959
thibaudj <thibaud.jacquemot@sncf.fr>
2021-01-05 11:49:30 +0100

    Exercice fait

--- src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/School.java
@@ -1,9 +1,9 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class School {
@@ -14,6 +14,9 @@ public class School {
     private String name;
     private Long capacity;
     private String country;
+
+    @OneToMany(mappedBy = "school", cascade = CascadeType.ALL)
+    private List<Wizard> wizards;
 
     public School() {
     }
@@ -48,5 +51,13 @@ public class School {
 
     public void setCountry(String country) {
         this.country = country;
+    }
+
+    public List<Wizard> getWizards() {
+        return wizards;
+    }
+
+    public void setWizards(List<Wizard> wizards) {
+        this.wizards = wizards;
     }
 }
